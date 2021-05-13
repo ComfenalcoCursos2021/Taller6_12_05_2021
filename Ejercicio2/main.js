e1c8bd3d0016097110f70690039c2cd0941f4650
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-05-12 21:16:56 -0500

    Ejercicio 2,3 con objetos

--- Ejercicio2/main.js
+++ Ejercicio2/main.js
@@ -6,4 +6,43 @@
 // •	Si lleva menos de 5 años, pero más que 3 se le aplica un aumento del 5%.
 // •	Si lleva menos de 3 años se le aplica un aumento del 3%.
 
-console.log("Conexion Exitosa");
+const anual = 14400000;
+const aumento = {
+    cantidad_anos: prompt('Ingrese la cantidada de años trabados'),
+    porcentajes: [
+        {
+            tiempo: 10, 
+            porcentaje: 0.10
+        },
+        {
+            tiempo: 5, 
+            porcentaje: 0.07
+        },
+        {
+            tiempo: 3, 
+            porcentaje: 0.05
+        },
+        {
+            tiempo: null, 
+            porcentaje: 0.03
+        },
+    ],
+    moneda: function ({...arg}){
+        return Intl.NumberFormat('ed-DE', {style: "currency", currency: "COP"}).format(arg.dinero);
+    },
+    porcentaje: function (){
+        let anos = this.cantidad_anos;
+        let i = 0;
+        while(!(anos > this.porcentajes[i].tiempo)){
+            i++;
+        }
+        this.salario = anual * anos;
+        this.totalaumento = anual * this.porcentajes[i].porcentaje;
+        return `
+            En ${anos} años gano un total de ${this.moneda({dinero: this.salario})} 
+            su aumento es de ${Math.fround((this.porcentajes[i].porcentaje*100))}% 
+            total a ganar el proximo año +${this.moneda({dinero: this.totalaumento})}`;
+    }
+}
+const datos = Object.create(aumento);
+console.log(datos.porcentaje());
--- Ejercicio3/main.js
+++ Ejercicio3/main.js
@@ -1,4 +1,21 @@
 // 3.Programa para solicitar el nombre, y la nota promedio de 
 // 5 estudiantes de un curso de computación.
 
-console.log("Conexion Exitosa");
+const curso = {
+    lista:  new Array(5).fill(null),
+    validar: function (){
+        for(let data in this.lista){
+            if(this.lista[data].promedio >=3) this.lista[data].mensaje = "Aprobo el curso de computación";
+            else this.lista[data].mensaje = "Reprobo el curso de computación";
+        }
+    }
+}
+const data = Object.create(curso);
+for(let i in data.lista){
+    data.lista[i] = {
+        nombre: prompt(`Ingrese el nombre del estudiante #${parseInt(i)+1}`),
+        promedio: prompt(`Ingrese el promedio del estudiante #${parseInt(i)+1}`),
+    };
+}
+data.validar();
+console.log(data.lista);
